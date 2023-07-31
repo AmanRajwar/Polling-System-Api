c7239b7cd01c3289b34a95db3d9772a1be158088
AmanRajwar <135263136+AmanRajwar@users.noreply.github.com>
2023-08-01 01:45:32 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router()
 
+router.get('/', (req,res)=>{
+  return res.send("<h1>Its Working</h2>");
+})
 router.use('/questions', require('./questions'))
 router.use('/options', require('./options'))
 
