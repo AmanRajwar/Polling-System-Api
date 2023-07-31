e21a640898579304a7417fb73c129697e4645386
AmanRajwar <135263136+AmanRajwar@users.noreply.github.com>
2023-08-01 01:37:23 +0530

    Update mongoose.js

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,6 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
- mongoose.connect('mongodb://127.0.0.1/polling_system');
+ const dbURI = process.env.MONGODB_URI;
+
+ mongoose.connect(dbURI);
 
  const db = mongoose.connection;
 
