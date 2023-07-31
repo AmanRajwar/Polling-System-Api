8974b671f0e895613ed73c366ae8e1fdcaa9d758
AmanRajwar <135263136+AmanRajwar@users.noreply.github.com>
2023-08-01 02:19:27 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const render = `<h1>Its Working</h1>
                   <h3> To use POST REQUEST You can use Postman</h3>
                    <a href="https://polling-system-api-sjb4.onrender.com/questions/64c81798949b1f107e86e643">GET QUESTION WITH ITS OPTIONS</a>
                    <P>REST IS UP TO YOU (●'◡'●) </P>`
+
+
 router.get('/', (req,res)=>{
   return res.send(render);
 })
